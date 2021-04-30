b04853d10dee4e0a6785245886a67f8c578d8a2b
Kasparov <ianjkiplagat@gmail.com>
2021-04-30 15:39:47 +0300

    add profile page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
-from flask import render_template,redirect,url_for, flash,request
-from flask_login import login_user,login_required,logout_user
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,login_required,logout_user,current_user
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
@@ -7,27 +7,45 @@ from . import auth
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+  
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index')) 
+      
     form = LoginForm()
+    
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
+        
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            next_page = request.args.get('next')
+         
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+            flash('User logged in', 'success')
+        
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
 
-        flash('Invalid username or Password')
-
-    title = "watchlist login"
+    title = "Sign In"
     return render_template('auth/login.html',form = form,title=title)
   
 @auth.route('/register',methods = ["GET","POST"])
 def register():
+  
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+                        
     form = RegistrationForm()
+    
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        
         db.session.add(user)
         db.session.commit()
+        
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('auth.login'))
-        title = "New Account"
+      
+        title = "Create Account"
     return render_template('auth/register.html',form = form)
   
 @auth.route('/logout')
@@ -35,6 +53,13 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+
+
+
+  
+ 
+  
 
 
   
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User
 
 
 @main.route('/')
@@ -14,3 +15,12 @@ def index():
     
     return render_template('index.html', title = title)
 
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
