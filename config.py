a0ce0dad4a0ce61b56f64f46733c9dbffbe61ecd
Kasparov <ianjkiplagat@gmail.com>
2021-05-05 22:22:04 +0300

    downgrade wekzeug

--- config.py
+++ config.py
@@ -17,7 +17,10 @@ class Config:
     
 
 class ProdConfig(Config):
-    pass
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
 
 
 class DevConfig(Config):
